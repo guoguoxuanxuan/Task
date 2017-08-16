62c078f87ad588cae691226a5622c07db0dc2b17
admin <admin@LAPTOP-ICCCVO5U>
2017-08-16 15:47:14 +0800

    按照一个基本的页面访问流程，调试spring boot mybatis web应用

--- crrcgo-task/src/main/java/com/crrcgo/task/CrrcgoTaskApplication.java
+++ crrcgo-task/src/main/java/com/crrcgo/task/CrrcgoTaskApplication.java
@@ -1,12 +1,24 @@
 package com.crrcgo.task;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@Controller
+@EnableWebMvc
 @SpringBootApplication
+@MapperScan(basePackages = "com.crrcgo.task.mapper")
 public class CrrcgoTaskApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrrcgoTaskApplication.class, args);
 	}
+	
+	 @RequestMapping("/")
+	    String home() {
+	        return "redirect:tasks";
+	    }
 }
